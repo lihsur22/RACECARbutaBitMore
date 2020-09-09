7da13ee8432a38b5a965819f43d60bf520d5e77c
lihsur22 <rushilganguli@outlook.com>
2020-09-09 16:46:02 +0530

    not asynchronous anymore

--- sketch.js
+++ sketch.js
@@ -1,29 +1,56 @@
 var ball;
+var db, pos;
 
 function setup(){
     createCanvas(500,500);
+
+    db = firebase.database();
+    var childPos = db.ref("ball/pos");
+    childPos.on("value",readDB,showErr);
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
 }
 
 function draw(){
     background("white");
-    if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+    if(pos !== undefined) 
+    {
+        if(keyDown(LEFT_ARROW))
+        {
+            writePosition(-4,0);
+        }
+        else if(keyDown(RIGHT_ARROW))
+        {
+            writePosition(4,0);
+        }
+        else if(keyDown(UP_ARROW))
+        {
+            writePosition(0,-4);
+        }
+        else if(keyDown(DOWN_ARROW))
+        {
+            writePosition(0,4);
+        }
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    db.ref("ball/pos").set({
+        x: ball.x + x,
+        y: ball.y + y
+    })
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+}
+
+function readDB(data) {
+pos = data.val();
+ball.x = pos.x;
+ball.y = pos.y;
+}
+
+function showErr() {
+    console.log("errorr");
 }
